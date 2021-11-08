50e622842c913604979b8c03f0e958710818edf3
moduloscript <maduemeka254@gmail.com>
2021-11-08 22:07:38 +0100

    The Trilogy Push

---
+++ components/Footer.js
@@ -0,0 +1,39 @@
+function Footer() {
+    return (
+        <div className="grid grid-cols-1 px-32 text-gray-600 bg-gray-100 gap-y-10 md:grid-cols-4 py-14">
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">ABOUT</h5>
+                <p>How Airbnb works</p>
+                <p>Newsroom</p>
+                <p>Investors</p>
+                <p>Airbnb Plus</p>
+                <p>Airbnb Luxe</p>
+            </div>
+            <div className="space-y-4 text-xs text-gray-800">
+            <h5 className="font-bold">COMMUNITY</h5>
+                <p>Graphql Africa</p>
+                <p>Oredo Connect</p>
+                <p>Africa Investment</p>
+                <p>Digi Co.</p>
+                <p>Liberal Code</p>
+            </div>
+            <div className="space-y-4 text-xs text-gray-800">
+            <h5 className="font-bold">HOST</h5>
+                <p>INVESTOPIA</p>
+                <p>Languine Code</p>
+                <p>RIA</p>
+                <p></p>
+            </div>
+            <div  className="space-y-4 text-xs text-gray-800" >
+            <h5 className="font-bold">SUPPORT</h5>
+                <p>Our Network Coverage</p>
+                <p>Promotionals</p>
+                <p>FrontDesk Assistants</p>
+                <p>Customer Care</p> 
+            </div>
+        </div>
+    )
+}
+
+export default Footer
+ 
